d0c50315d5d8f8c9aee111b40ba4938914b65c96
王晨 <271724646@qq.com>
2019-07-25 16:56:13 +0800

    使用sentinel 1.6以上监控。引入getway网关

--- cloud-fegin/src/main/java/com/example/cloudfegin/controller/ClientTwoController.java
+++ cloud-fegin/src/main/java/com/example/cloudfegin/controller/ClientTwoController.java
@@ -3,6 +3,7 @@ package com.example.cloudfegin.controller;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.example.cloudfegin.BlockHander;
 import com.example.cloudfegin.feginclient.ClientTwoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/7/12 16:26
  * @Version 1.0
  */
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/client")
 public class ClientTwoController {
 
     @Autowired
     private ClientTwoService clientTwoService;
-
 
     @GetMapping(value = "/getData")
     public String test(){
@@ -30,9 +32,9 @@ public class ClientTwoController {
     }
 
     @GetMapping(value = "/getData2")
-    @SentinelResource(blockHandlerClass = BlockHander.class)
     public String test2(){
 
+        log.info("测试@Slf4j");
         return clientTwoService.getData2();
     }
 }
